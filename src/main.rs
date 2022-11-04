d4f14fe6ec7f12fc771491e83df1244179eb0908
aleksei.novikov <aleksei.novikov@onoffapp.com>
2022-11-04 13:22:06 +0200

    Some things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8
+}
+
+impl Person {
+    fn new(name: String, age: u8) -> Person {
+        Person { name: name, age: age }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let leha = Person::new("Leha".to_owned(), 30);
+    println!("{:#?}", leha);
 }
